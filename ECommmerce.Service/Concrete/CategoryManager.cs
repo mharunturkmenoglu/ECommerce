37afe49cfbedad606c809c032ec60b3bd7ebf3d7
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-11 01:21:23 +0300

    Managers
    managers are created.

--- ECommmerce.Service/Concrete/CategoryManager.cs
+++ ECommmerce.Service/Concrete/CategoryManager.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using ECommerce.Data.Abstract;
+using ECommerce.Entities.Concrete;
+using ECommmerce.Service.Abstract;
+using Microsoft.Extensions.Configuration;
 
 namespace ECommmerce.Service.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
+        private readonly IConfiguration _config;
+        private readonly IAdoNetDataReader _adoNetDataReader;
+
+        public CategoryManager(IConfiguration config, IAdoNetDataReader adoNetDataReader)
+        {
+            _config = config;
+            _adoNetDataReader = adoNetDataReader;
+        }
+
+        public void Add(Category category, string createdByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int categoryID, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            var categoryList = new List<Category>();
+            var queryScript = "select * from dbo.Categories where [Id] = 1";
+
+            var dataReader = _adoNetDataReader.GetDataReader(queryScript);
+            while (dataReader.Read())
+            {
+                var category = new Category();
+                category.Id = 1;
+                category.Name = (string)dataReader["Name"];
+                category.Description = (string)dataReader["Description"];
+                category.IsDeleted = true;
+                category.IsActive = true;
+                category.CreatedDate = DateTime.Now;
+                category.ModifiedDate = DateTime.Now;
+                category.CreatedByName = (string)dataReader["CreatedByName"];
+                category.ModifiedByName = (string)dataReader["ModifiedByName"];
+                category.Note = "note";
+                categoryList.Add(category);
+            }
+        }
+
+        public void GetAllByNonDeleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllByNonDeletedAndActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HardDelete(int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category, string modifiedByName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
