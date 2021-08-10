343e20d86580c39d4368a5f44943f6208511878e
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-11 01:02:05 +0300

    Update DataAccess
    Update DataAccess

---
+++ ECommerce.Data/Concrete/AdoNetDataReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Data.Abstract;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommerce.Data.Concrete
+{
+    public class AdoNetDataReader : IAdoNetDataReader
+    {
+        private readonly IConfiguration _config;
+
+        public AdoNetDataReader(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlDataReader GetDataReader(string queryScript)
+        {
+            var connectionString = _config.GetConnectionString("LocalDB");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(queryScript, connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+            return dataReader;
+        }
+    }
+}
