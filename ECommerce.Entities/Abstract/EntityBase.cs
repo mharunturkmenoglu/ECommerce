c3e73a9719361e0af4d20678c1b647afecba9f14
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-12 15:23:41 +0300

    Update EntityBase.cs
    EntityBase is updated.

--- ECommerce.Entities/Abstract/EntityBase.cs
+++ ECommerce.Entities/Abstract/EntityBase.cs
@@ -10,12 +10,12 @@ namespace ECommerce.Entities.Abstract
     {
         public virtual int Id { get; set; }
         public virtual string Description { get; set; }
-        public virtual bool IsDeleted { get; set; } = false;
-        public virtual bool IsActive { get; set; } = true;
-        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
-        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
-        public virtual string CreatedByName { get; set; } = "admin";
-        public virtual string ModifiedByName { get; set; } = "admin";
+        public virtual bool IsDeleted { get; set; }
+        public virtual bool IsActive { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
         public virtual string Note { get; set; }
     }
 }
