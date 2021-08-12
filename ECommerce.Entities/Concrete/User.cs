a07697d5b198659f727e3941df6c579ba9cc02ac
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-12 15:11:21 +0300

    Update user.cs
    password property is updated string from byte array.

--- ECommerce.Entities/Concrete/User.cs
+++ ECommerce.Entities/Concrete/User.cs
@@ -11,6 +11,6 @@ namespace ECommerce.Entities.Concrete
     {
         public string UserName { get; set; }
         public string Email { get; set; }
-        public byte[] Password { get; set; }
+        public string Password { get; set; }
     }
 }
