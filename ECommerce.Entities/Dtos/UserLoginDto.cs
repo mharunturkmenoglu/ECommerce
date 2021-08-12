bd0def26664648317c02d472dee5472cdbeef57f
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-12 15:14:45 +0300

    Update UserLoginDto
    Data annotations are added.

--- ECommerce.Entities/Dtos/UserLoginDto.cs
+++ ECommerce.Entities/Dtos/UserLoginDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,13 @@ namespace ECommmerce.Entities.Dtos
     public class UserLoginDto
     {
         [Required]
+        [EmailAddress(ErrorMessage = "Mail Adresiniz gecerli degil.")]
+        [DisplayName("Mail")]
         public string Email { get; set; }
         [Required]
+        [MaxLength(20,ErrorMessage = "Sifreniz maksimum 20 karakterden fazla olamaz.")]
+        [MinLength(3,ErrorMessage = "Sifreniz minimum 3 karakterden fazla olamaz.")]
+        [DisplayName("Sifreniz")]
         public string Password { get; set; }
     }
 }
