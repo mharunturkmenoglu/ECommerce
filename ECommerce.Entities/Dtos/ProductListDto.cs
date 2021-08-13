7eb429bb3fedc09e2d62d72e26aee4664a02eadd
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-13 11:26:19 +0300

    Create ProductListDto.cs
    ProductListDto is created.

---
+++ ECommerce.Entities/Dtos/ProductListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Entities.Concrete;
+
+namespace ECommmerce.Entities.Dtos
+{
+    public class ProductListDto
+    {
+        public List<Product> Articles { get; set; }
+    }
+}
