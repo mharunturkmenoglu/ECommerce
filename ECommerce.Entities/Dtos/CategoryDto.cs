3f8c8a7c1444bdd027b7528cbfac7edc1983864f
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-13 17:12:19 +0300

    Create CategoryDto.cs
    created.

---
+++ ECommerce.Entities/Dtos/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Entities.Concrete;
+
+namespace ECommmerce.Entities.Dtos
+{
+    public class CategoryDto
+    {
+        public Category Category { get; set; }
+    }
+}
