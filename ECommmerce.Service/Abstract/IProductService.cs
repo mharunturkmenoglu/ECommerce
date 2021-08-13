e28e0e0e8661d06b84d2bd26383e45dd193677c5
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-13 11:30:36 +0300

    Update IProductService.cs
    IProductService is updated.

--- ECommmerce.Service/Abstract/IProductService.cs
+++ ECommmerce.Service/Abstract/IProductService.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ECommerce.Entities.Concrete;
+using ECommmerce.Entities.Dtos;
+using ECommmerce.Service.Results.Abstract;
 
 namespace ECommmerce.Service.Abstract
 {
     public interface IProductService
     {
-        Product Get(int productId);
-        List<Product> GetAll();
-        List<Product> GetAllByNonDeleted();
-        List<Product> GetAllByNonDeletedAndActive();
-        List<Product> GetAllByCategory(int categoryId);
-        void Add(Product product, string createdByName);
-        void Update(Product product, string modifiedByName);
-        void Delete(int productId, string modifiedByName);
-        void HardDelete(int productId);
+        public IDataResult<ProductDto> Get(int productId);
+        public IDataResult<ProductListDto> GetAll();
+        public IDataResult<ProductListDto> GetAllByNonDeleted();
+        public IDataResult<ProductListDto> GetAllByNonDeletedAndActive();
+        public IDataResult<ProductListDto> GetAllByCategory(int categoryId);
+        public IDataResult<ProductDto> Add(Product product, string createdByName);
+        public IDataResult<ProductDto> Update(Product product, string modifiedByName);
+        public IDataResult<ProductDto> Delete(int productId, string modifiedByName);
+        public IDataResult<ProductDto> HardDelete(int productId);
     }
 }
