9d504474561bca03eeccb41df8e534c44f9bbbf6
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-13 23:00:31 +0300

    Create Authentication
    create : homemade auth.

---
+++ ECommmerce.Service/Authentication/AuthenticationManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Entities.Concrete;
+using ECommmerce.Service.Authentication;
+
+namespace ECommerce.Service.Authentication
+{
+    public class AuthenticationManager : IAuthenticationService
+    {
+        private User user;
+        public Languages CurrentLanguage { get; set; } = Languages.English;
+
+
+        public AuthenticationManager(User user)
+        {
+            this.user = user;
+        }
+        public void Authenticate(User user)
+        {
+            this.user = user;
+        }
+
+        public User GetCurrentUser()
+        {
+            return user;
+        }
+
+        public void SetLanguage(int id)
+        {
+            this.CurrentLanguage = (Languages)Enum.ToObject(typeof(Languages), id);
+        }
+
+        public Languages GetLanguage()
+        {
+            return this.CurrentLanguage;
+        }
+    }
+}
---
+++ ECommmerce.Service/Authentication/IAuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using ECommerce.Entities.Concrete;
+using ECommmerce.Service.Authentication;
+
+namespace ECommerce.Service.Authentication
+
+{
+    public interface IAuthenticationService
+    {
+        public void Authenticate(User user);
+        public User GetCurrentUser();
+        public void SetLanguage(int id);
+        public Languages GetLanguage();
+    }
+}
