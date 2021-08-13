509203abf4df2acb1cbc0dde9e7de42daea69ce6
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-13 11:25:16 +0300

    Create DataResult Model
    DataResult Model is created.

---
+++ ECommmerce.Service/Results/Abstract/IDataResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommmerce.Service.Results.Concrete;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+
+namespace ECommmerce.Service.Results.Abstract
+{
+    public interface IDataResult<out T> : IResult
+    {
+        public T Data { get; }
+    }
+}
---
+++ ECommmerce.Service/Results/Concrete/DataResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommmerce.Service.Results.Abstract;
+
+namespace ECommmerce.Service.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public T Data { get; set; }
+        public ResultStatus ResultStatus { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+        }
+    }
+}
